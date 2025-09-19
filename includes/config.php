f510e2c4cea7076d3d25d599d71e50c70741d87e
AymanSedqi <sedqi.ayman@groupe-agel.com>
2025-09-19 15:02:23 +0100

    first commit

---
+++ includes/config.php
@@ -0,0 +1,9 @@
+<?php
+$host = 'bmr84iyafk6bdlarsi2o-mysql.services.clever-cloud.com';
+$dbuser = 'ud8xtmduwxctkr4c';
+$dbpass = 'Mu0UCwfBkjNebc97CbND';
+$db = 'bmr84iyafk6bdlarsi2o';
+$port = 3306;
+
+$mysqli = new mysqli($host, $dbuser, $dbpass, $db, $port);
+?>
